504f169b48ebf3af3d59c9562865fb004d0dd7b6
Jedi of Javascript <40812932+jriffs@users.noreply.github.com>
2026-07-17 14:57:25 +0000

    day 65 done

--- state-counter/programs/state-counter/src/lib.rs
+++ state-counter/programs/state-counter/src/lib.rs
@@ -1,21 +1,65 @@
-pub mod constants;
-pub mod error;
-pub mod instructions;
-pub mod state;
-
 use anchor_lang::prelude::*;
-
-pub use constants::*;
-pub use instructions::*;
-pub use state::*;
 
 declare_id!("7m6qUKkcYZBW9AbxcttnxT9pjNPPhVH2Epm2JE8Addoa");
 
 #[program]
-pub mod state_counter {
+pub mod counter {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn init_counter(ctx: Context<InitCounter>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.user = ctx.accounts.user.key();
+        counter.count = 0;
+        counter.bump = ctx.bumps.counter;
+        Ok(())
     }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = counter
+            .count
+            .checked_add(1)
+            .ok_or(CounterError::Overflow)?;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitCounter<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 8 + Counter::INIT_SPACE,
+        seeds = [b"counter", user.key().as_ref()],
+        bump
+    )]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(
+        mut,
+        seeds = [b"counter", user.key().as_ref()],
+        bump = counter.bump,
+    )]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Counter {
+    pub user: Pubkey,
+    pub count: u64,
+    pub bump: u8,
+}
+
+#[error_code]
+pub enum CounterError {
+    #[msg("counter overflow")]
+    Overflow,
 }
